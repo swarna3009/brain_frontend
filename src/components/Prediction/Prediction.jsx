f95baec0b435db4308930b9a99f4d1ae94a27b62
swarna3009 <145216193+swarna3009@users.noreply.github.com>
2025-06-20 21:35:07 +0530

    Update Prediction.jsx

--- src/components/Prediction/Prediction.jsx
+++ src/components/Prediction/Prediction.jsx
@@ -31,7 +31,7 @@ const Prediction = () => {
     setResult(null);
 
     try {
-      const response = await fetch("http://localhost:5000/predict", {
+      const response = await fetch("https://backend-brain-1.onrender.com/predict", {
         method: "POST",
         body: formData,
       });
