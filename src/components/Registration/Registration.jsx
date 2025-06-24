047117e4f9f5b298173912c561672bec03524693
swarna3009 <145216193+swarna3009@users.noreply.github.com>
2025-06-24 12:30:03 +0530

    Update Registration.jsx

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -46,7 +46,7 @@ const Register = () => {
     }
 
     try {
-      const response = await fetch("https://backend-brain-1.onrender.com/user-register", {
+      const response = await fetch("https://backend-brain-2.onrender.com/user-register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password })
