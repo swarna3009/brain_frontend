f1610b1c18989c48c90494ab6cb8d1e0ade187f3
swarna3009 <145216193+swarna3009@users.noreply.github.com>
2025-06-24 19:03:29 +0530

    Update Prediction.jsx

--- src/components/Prediction/Prediction.jsx
+++ src/components/Prediction/Prediction.jsx
@@ -22,8 +22,9 @@ const Prediction = () => {
 
   const handleUpload = async (e) => {
     e.preventDefault();
-     const storedEmail = localStorage.getItem("userEmail");
-    if (!selectedFile) return;
+    const storedEmail = localStorage.getItem("userEmail");
+    if (!selectedFile) return alert("choose file ");
+    if(!storedEmail) return alert("plz register or log in first");
 
     const formData = new FormData();
     formData.append("file", selectedFile);
