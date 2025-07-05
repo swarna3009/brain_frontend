2ce9ae3c256a1176318d4b23fbd987d5efdd04c1
swarna3009 <145216193+swarna3009@users.noreply.github.com>
2025-07-05 20:06:03 +0530

    Update Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -54,7 +54,7 @@ const Login = ({ role = "user" }) => {
   return (
     <div
       className="relative min-h-screen bg-cover top-5 bg-center text-white font-nunito"
-      style={{ backgroundImage: url('/assets/brain7.jpg') }}
+      style={{ backgroundImage: `url('/assets/brain7.jpg')` }}
     >
       {/* Background Blur */}
       <div className="absolute inset-0 bg-opacity-40 backdrop-blur-sm" />
