c8d19bb0fb261eb0d417dc842753b403682d169f
swarna3009 <145216193+swarna3009@users.noreply.github.com>
2026-01-13 18:07:38 +0530

    Update AdminLogin.jsx

--- src/components/Admin/AdminLogin.jsx
+++ src/components/Admin/AdminLogin.jsx
@@ -14,7 +14,7 @@ const AdminLogin = () => {
     setMessage("");
 
     try {
-      const response = await fetch("https://backend-brain-1.onrender.com/admin-login", {
+      const response = await fetch("https://backend-brain1.onrender.com/admin-login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
